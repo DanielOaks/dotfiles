aab76eaec32aae2ea77c0513f3bfe1f925572050
Daniel Oaks <daniel@danieloaks.net>
2019-02-14 13:02:00 +1000

    update

--- config.fish
+++ config.fish
@@ -2,6 +2,8 @@
 # set go path
 set PATH $PATH ~/go/bin
 
+status --is-interactive; and source (rbenv init -|psub)
+
 # attach to or create new tmux session
 if status is-interactive
 and not set -q TMUX
